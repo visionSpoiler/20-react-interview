670b8068329875492be0387a6048f14bafb56d03
unknown <wlals7565@naver.com>
2025-06-18 12:43:56 +0900

    [구현]: 커스텀 훅 useOutsideClick

--- src/components/use-outside-click/index.jsx
+++ src/components/use-outside-click/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const useOutsideClick = () => {
-  return (
-    <div>index</div>
-  )
+const useOutsideClick = (ref, handler) => {
+  useEffect(() => {
+    const listener = (e) => {
+      if(!ref.current || ref.current.contains(e.target)) {
+        return 
+      }
+
+      handler(e)
+    }
+
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
+
+    return () => {
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [handler, ref])
 }
 
 export default useOutsideClick
--- src/components/use-outside-click/test.jsx
+++ src/components/use-outside-click/test.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import useOutsideClick from ".";
 
 const TestUseOutSideClick = () => {
   const [showContent, setShowContent] = useState(false);
+  const ref = useRef()
+  useOutsideClick(ref, () => {
+    setShowContent(false)
+  })
 
   return (
-    <div onClick={}>
+    <div>
       {showContent ? (
-        <div onClick={}>
+        <div ref={ref} >
           <h1>This is a random content</h1>
           <p>
             Please click outside of this to close this. it won't close if you
